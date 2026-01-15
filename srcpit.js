0fdb213a659518589975910f2656e9f174a7b3f6
aswani-hub-byte <ash2selvi@gmail.com>
2026-01-16 02:07:12 +0530

    Initial commit

---
+++ srcpit.js
@@ -0,0 +1,20 @@
+﻿
+// ============================================
+// 3D COLOR CHANGE FUNCTIONALITY
+// ============================================
+
+function sendConfig() {
+  const iframe = document.getElementById("modelFrame");
+  const IFRAME_ORIGIN = "http://127.0.0.1:5500/"; 
+  iframe.contentWindow.postMessage(
+    {
+      type: "CONFIG_UPDATE",
+      color: "red"
+    },
+    IFRAME_ORIGIN
+  );
+}
+
+window.addEventListener("message", (event) => {
+  console.log("Message from iframe:", event.data);
+});
